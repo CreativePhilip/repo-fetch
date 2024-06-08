af5576a9a43f0b52646bf20778090cf13073b3fd
Philip Policki <philip.policki@gmail.com>
2024-06-08 11:44:13 +0200

    feat: mini refactor of sync code

--- src/services/sync/entrypoint.go
+++ src/services/sync/entrypoint.go
@@ -30,8 +30,6 @@ func Sync(ctx *cli.Context) error {
 		return err
 	}
 
-	fmt.Println("Config: ", cfg)
-
 	gitClient, err := clients.CreateProviderFromConfig(cfg)
 
 	if err != nil {
@@ -48,23 +46,12 @@ func Sync(ctx *cli.Context) error {
 	sem := semaphore.NewWeighted(10)
 
 	for _, repo := range repos {
-		if strings.HasPrefix(repo.Path, cfg.GroupWhitelist[0]) {
-			wg.Add(1)
-
-			go func(repository *clients.GitRepository) {
-				_ = sem.Acquire(context.Background(), 1)
-				defer sem.Release(1)
-				defer wg.Done()
-				output, err := handleRepo(cfg, repository)
-
-				if err != nil {
-					fmt.Println(err)
-				} else {
-					fmt.Println(output)
-				}
-
-			}(repo)
+		if !strings.HasPrefix(repo.Path, cfg.GroupWhitelist[0]) {
+			continue
 		}
+
+		wg.Add(1)
+		go handleRepoAsync(cfg, repo, &wg, sem)
 	}
 
 	wg.Wait()
@@ -72,25 +59,25 @@ func Sync(ctx *cli.Context) error {
 	return nil
 }
 
+func handleRepoAsync(cfg *config.RfetchConfig, repo *clients.GitRepository, wg *sync.WaitGroup, sem *semaphore.Weighted) {
+	_ = sem.Acquire(context.Background(), 1)
+	defer sem.Release(1)
+	defer wg.Done()
+	output, err := handleRepo(cfg, repo)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(output)
+	}
+}
+
 func handleRepo(cfg *config.RfetchConfig, repo *clients.GitRepository) (string, error) {
 	repoPath := path.Join(cfg.GetRootPathAbs(), repo.Path)
 
+	// Path does not exist, therefore repository does not exist, needs cloning
 	if _, err := os.Stat(repoPath); errors.Is(err, fs.ErrNotExist) {
-		parentPath := path.Dir(repoPath)
-		err := os.MkdirAll(parentPath, os.ModePerm)
-
-		if err != nil {
-			return "", err
-		}
-
-		err = os.Chdir(path.Dir(repoPath))
-
-		if err != nil {
-			return "", err
-		}
-
-		cmd := exec.Command("git", "clone", repo.SshUrl)
-		err = cmd.Run()
+		err = cloneRepo(repo, path.Dir(repoPath))
 
 		if err != nil {
 			return "", err
@@ -99,18 +86,37 @@ func handleRepo(cfg *config.RfetchConfig, repo *clients.GitRepository) (string, 
 		return "Cloned Repository: " + repo.Path, nil
 	}
 
-	err := os.Chdir(repoPath)
-
-	if err != nil {
-		return "", err
-	}
-
-	cmd := exec.Command("git", "fetch", "--all")
-	err = cmd.Run()
+	err := updateRepo(repo, repoPath)
 
 	if err != nil {
 		return "", err
 	}
 
 	return "Updated repository: " + repo.Path, nil
+}
+
+func cloneRepo(repo *clients.GitRepository, repoPath string) error {
+	err := os.MkdirAll(repoPath, os.ModePerm)
+
+	if err != nil {
+		return err
+	}
+
+	err = os.Chdir(repoPath)
+
+	if err != nil {
+		return err
+	}
+
+	return exec.Command("git", "clone", repo.SshUrl).Run()
+}
+
+func updateRepo(repo *clients.GitRepository, repoPath string) error {
+	err := os.Chdir(repoPath)
+
+	if err != nil {
+		return err
+	}
+
+	return exec.Command("git", "fetch", "--all").Run()
 }
